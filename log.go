09f66292908c002862d06c42ae438e6070c590d6
nhnghia272 <nhnghia272@gmail.com>
2024-09-30 19:10:53 +0700

    v1.0.7

--- log.go
+++ log.go
@@ -54,3 +54,35 @@ func Warnf(format string, args ...any) {
 func Infof(format string, args ...any) {
 	logrus.Infof(format, args...)
 }
+
+type Log struct {
+	entry *logrus.Entry
+}
+
+func Fields(kv map[string]any) *Log {
+	return &Log{logrus.WithFields(logrus.Fields(kv))}
+}
+
+func (s Log) Error(args ...any) {
+	s.entry.Errorln(args...)
+}
+
+func (s Log) Warn(args ...any) {
+	s.entry.Warnln(args...)
+}
+
+func (s Log) Info(args ...any) {
+	s.entry.Infoln(args...)
+}
+
+func (s Log) Errorf(format string, args ...any) {
+	s.entry.Errorf(format, args...)
+}
+
+func (s Log) Warnf(format string, args ...any) {
+	s.entry.Warnf(format, args...)
+}
+
+func (s Log) Infof(format string, args ...any) {
+	s.entry.Infof(format, args...)
+}
